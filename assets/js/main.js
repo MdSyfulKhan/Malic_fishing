0e38087a135d4bac394fea3c95d8d80c442d0a6d
MdSyfulKhan <mdsyfulkhan9899@gmail.com>
2022-03-29 16:07:07 +0600

    updates

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+$(".slick-slide-in").slick({
+  slideToShow: 1,
+  slideScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  Infinity: true,
+  fade: true,
+  prevArrow: '<i class="fa-solid fa-angle-left left-arrow"></i>',
+  nextArrow: '<i class="fa-solid fa-angle-right right-arrow"></i>',
+});
+//brand slider
+$(".brand-slider").slick({
+  slidesToShow: 6,
+  slidesToScroll: 3,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow: "",
+  nextArrow: "",
+  Infinity: true,
+});
+
+//product filtering
+// init Isotope
+var $grid = $(".st-product-filter").isotope({
+  // options
+});
+// filter items on li click
+$(".st-category-items").on("click", "li", function () {
+  var filterValue = $(this).attr("data-filter");
+  $grid.isotope({ filter: filterValue });
+});
+//filter item on li active
+$('.st-category-items li').click(function(){
+  $('.st-category-items li').removeClass('active');
+  $(this).addClass('active');
+});
