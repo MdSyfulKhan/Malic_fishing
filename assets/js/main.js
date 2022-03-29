373403ef245c2e4632492a8be3a985910939c813
tamjidmurtoza <tamjid.murtoza@gmail.com>
2022-03-29 16:51:45 +0600

    slider issue done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,52 @@
-$(".slick-slide-in").slick({
+$('.slick-slide-in').slick({
   slideToShow: 1,
   slideScroll: 1,
   autoplay: true,
-  autoplaySpeed: 3000,
+  autoplaySpeed: 4000,
   Infinity: true,
   fade: true,
   prevArrow: '<i class="fa-solid fa-angle-left left-arrow"></i>',
   nextArrow: '<i class="fa-solid fa-angle-right right-arrow"></i>',
 });
 //brand slider
-$(".brand-slider").slick({
+$('.brand-slider').slick({
   slidesToShow: 6,
   slidesToScroll: 3,
   autoplay: true,
-  autoplaySpeed: 2000,
-  prevArrow: "",
-  nextArrow: "",
+  autoplaySpeed: 4000,
+  prevArrow: '',
+  nextArrow: '',
+  Infinity: true,
+});
+$('.gallery-slider').slick({
+  slidesToShow: 5,
+  slidesToScroll: 3,
+  autoplay: false,
+  prevArrow: '',
+  nextArrow: '',
+  Infinity: true,
+});
+$('.team-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  autoplay: false,
+  prevArrow: '',
+  nextArrow: '',
   Infinity: true,
 });
 
 //product filtering
 // init Isotope
-var $grid = $(".st-product-filter").isotope({
+var $grid = $('.st-product-filter').isotope({
   // options
 });
 // filter items on li click
-$(".st-category-items").on("click", "li", function () {
-  var filterValue = $(this).attr("data-filter");
+$('.st-category-items').on('click', 'li', function () {
+  var filterValue = $(this).attr('data-filter');
   $grid.isotope({ filter: filterValue });
 });
 //filter item on li active
-$('.st-category-items li').click(function(){
+$('.st-category-items li').click(function () {
   $('.st-category-items li').removeClass('active');
   $(this).addClass('active');
 });
