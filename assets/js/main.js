f0f16034812cd854c9b2370e366058f66ebed5a8
MdSyfulKhan <mdsyfulkhan9899@gmail.com>
2022-03-30 11:22:02 +0600

    add popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,3 +50,26 @@ $('.st-category-items li').click(function () {
   $('.st-category-items li').removeClass('active');
   $(this).addClass('active');
 });
+
+//product popup
+$('.p-img').magnificPopup({
+  type: 'image',
+});
+//video play pupup
+$('.st-play-btn').magnificPopup({
+  type: 'iframe',
+  iframe: {
+    markup: '<div class="mfp-iframe-scaler">'+
+              '<div class="mfp-close"></div>'+
+              '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+            '</div>', 
+    patterns: {
+      youtube: {
+        index: 'youtube.com/',
+        id: 'v=',  
+        src: 'https//www.youtube.com/embed/%id%?autoplay=1'
+      }, 
+    },
+    srcAction: 'iframe_src',
+  }
+})
