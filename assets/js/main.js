9c22f74280be92e6f8ad25d1204dbfd3d4f8a35b
MdSyfulKhan <mdsyfulkhan9899@gmail.com>
2022-03-31 14:42:18 +0600

    popup not working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-$('.st-nav-list li').click(function(){
-  $('.st-nav-list li').removeClass('active');
-  $(this).addClass('active');
+$(".st-nav-list li").click(function () {
+  $(".st-nav-list li").removeClass("active");
+  $(this).addClass("active");
 });
 //hero
-$('.slick-slide-in').slick({
+$(".slick-slide-in").slick({
   slideToShow: 1,
   slideScroll: 1,
   autoplay: true,
@@ -14,68 +14,68 @@ $('.slick-slide-in').slick({
   nextArrow: '<i class="fa-solid fa-angle-right right-arrow"></i>',
 });
 //brand slider
-$('.brand-slider').slick({
+$(".brand-slider").slick({
   slidesToShow: 6,
   slidesToScroll: 3,
   autoplay: true,
   autoplaySpeed: 4000,
-  prevArrow: '',
-  nextArrow: '',
+  prevArrow: "",
+  nextArrow: "",
   Infinity: true,
 });
-$('.gallery-slider').slick({
+$(".gallery-slider").slick({
   slidesToShow: 5,
   slidesToScroll: 3,
   autoplay: false,
-  prevArrow: '',
-  nextArrow: '',
+  prevArrow: "",
+  nextArrow: "",
   Infinity: true,
 });
-$('.team-slider').slick({
+$(".team-slider").slick({
   slidesToShow: 3,
   slidesToScroll: 3,
   autoplay: false,
-  prevArrow: '',
-  nextArrow: '',
+  prevArrow: "",
+  nextArrow: "",
   Infinity: true,
-  dots:true,
+  dots: true,
 });
 
 //product filtering
 // init Isotope
-var $grid = $('.st-product-filter').isotope({
+var $grid = $(".st-product-filter").isotope({
   // options
 });
 // filter items on li click
-$('.st-category-items').on('click', 'li', function () {
-  var filterValue = $(this).attr('data-filter');
+$(".st-category-items").on("click", "li", function () {
+  var filterValue = $(this).attr("data-filter");
   $grid.isotope({ filter: filterValue });
 });
 //filter item on li active
-$('.st-category-items li').click(function () {
-  $('.st-category-items li').removeClass('active');
-  $(this).addClass('active');
+$(".st-category-items li").click(function () {
+  $(".st-category-items li").removeClass("active");
+  $(this).addClass("active");
 });
 
 //product popup
-$('.p-img').magnificPopup({
-  type: 'image',
+$(".p-img").magnificPopup({
+  type: "image",
 });
 //video play pupup
-$('.st-play-btn').magnificPopup({
-  type: 'iframe',
-  iframe: {
-    markup: '<div class="mfp-iframe-scaler">'+
-              '<div class="mfp-close"></div>'+
-              '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-            '</div>', 
-    patterns: {
-      youtube: {
-        index: 'youtube.com/',
-        id: 'v=',  
-        src: 'https//www.youtube.com/embed/%id%?autoplay=1'
-      }, 
-    },
-    srcAction: 'iframe_src',
-  }
-})
+function modalVideo() {
+  $(document).on("click", ".st-video-open", function (e) {
+    e.preventDefault();
+    var video = $(this).attr("href");
+    $(".st-video-popup-container iframe").attr("src", video);
+    $(".st-video-popup").addClass("active");
+  });
+  $(".st-video-popup-close, .st-video-popup-layer").on(
+    "click",
+    function (e) {
+      $(".st-video-popup").removeClass("active");
+      $("html").removeClass("overflow-hidden");
+      $(".st-video-popup-container iframe").attr("src", "about:blank");
+      e.preventDefault();
+    }
+  );
+}
